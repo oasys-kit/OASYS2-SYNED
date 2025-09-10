e941ddb8c7883ced6c46a42a881218f1a0ab2bfe
lrebuffi <lrebuffi@anl.gov>
2025-09-10 12:17:29 -0500

    Preparation for OASYS 2

---
+++ orangecontrib/syned/widgets/beamline_elements/ow_screen.py
@@ -0,0 +1,25 @@
+from oasys2.canvas.util.oasys_util import add_parameter_to_module
+
+from orangecontrib.syned.widgets.gui.ow_optical_element import OWOpticalElement
+
+from syned.beamline.optical_elements.ideal_elements.screen import Screen
+
+class OWScreen(OWOpticalElement):
+
+    name = "Screen"
+    description = "Syned: Screen"
+    icon = "icons/screen.png"
+    priority = 4
+
+    def __init__(self):
+        super().__init__(allow_angle_radial=False, allow_angle_azimuthal=False)
+
+    def draw_specific_box(self):
+        pass
+
+    def get_optical_element(self):
+        return Screen(name=self.oe_name)
+
+
+add_parameter_to_module(__name__, OWScreen)
+
