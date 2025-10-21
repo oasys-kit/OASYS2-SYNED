0a36fce6670d7bd1615bf9b755f9ec5a23d8d385
lrebuffi <lrebuffi@anl.gov>
2025-10-21 14:32:50 -0500

    Escape on text Fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS2-SYNED'
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 ISRELEASED = False
 
 DESCRIPTION = 'SYNED SYNchrotron Elements Dictionary'
